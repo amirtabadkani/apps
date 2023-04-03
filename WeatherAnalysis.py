781560e425c4e39bd58a87137e66c2d9446858af
Amir Tabadkani <amirtabadkani@yahoo.com>
2023-04-03 13:57:34 +1000

    Update WeatherAnalysis.py

--- WeatherAnalysis.py
+++ WeatherAnalysis.py
@@ -26,6 +26,9 @@ from ladybug.analysisperiod import AnalysisPeriod
 
 st.set_page_config(page_title='EPW File Reader', layout='wide')
 
+with st.container.image('https://1731290220-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2FZbSo9tzMwlZsIB61RpcZ%2Fuploads%2FbKSSyj7avm80wP7Ymqew%2FEPW%20Map.png?alt=media&token=517fc466-a693-4007-98a8-c35c6a97f8f6')
+    
+    
 with st.sidebar:
     st.header('__EPW File Reader__')
     st.markdown('_Developed by **Amir Tabadkani**, \nPh.D. Computational Design Lead, Sustainability_')
